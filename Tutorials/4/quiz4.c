94b42af41740dab8723da4870b240c8428ee9f18
Alexander Whillas <whillas@gmail.com>
2013-04-15 12:50:08 +1000

    Quiz 4. Done.

--- Tutorials/4/quiz4.c
+++ Tutorials/4/quiz4.c
@@ -22,6 +22,44 @@ int main(void) {
     int c;
     while ((c = getchar()) != '\n') {
         /* REPLACE THIS COMMENT WITH YOUR CODE */
+
+        if(ispunct(c)) {
+            spacing_between_words = smallest_digit;
+            max_nb_of_words_to_display = largest_digit;
+            smallest_digit = MIN;
+            largest_digit = MAX;
+            if(words_displayed) {
+                putchar('\n');
+                words_displayed = false;
+                processing_a_word = false;
+            }
+        } else if (isdigit(c)) {
+            if (c - '0' < smallest_digit)
+                smallest_digit = c -'0';
+            if (c - '0' > largest_digit)
+                largest_digit = c - '0';
+        } 
+        if (isalpha(c)) {
+            // Beginning of a word
+            if (max_nb_of_words_to_display > 0) {
+                words_displayed = true;
+                if(processing_a_word) {
+                    putchar(tolower(c));
+                }
+                else {
+                    putchar(toupper(c));
+                    processing_a_word = true;
+                }
+            }
+        } else {
+            // End of a word.
+            if (processing_a_word) {
+                processing_a_word = false;
+                max_nb_of_words_to_display--;
+                for(int i = 0; i < spacing_between_words; ++i)
+                    putchar(' ');
+            }
+        }
     }
     if (words_displayed)
         putchar('\n');
