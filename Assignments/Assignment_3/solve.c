c498c657830cabb7ee709a80fe9fa16bfec82208
Alexander Whillas <whillas@gmail.com>
2013-05-26 12:32:54 +1000

    Renamed to solve.c. Debugging Sirs in statment counter

--- Assignments/Assignment_3/solve.c
+++ Assignments/Assignment_3/solve.c
@@ -11,10 +11,20 @@
 #include <string.h>
 
 // Sir listing type.
-#define AND_KNIGHTS 0
-#define OR_KNIGHTS 1
-#define AND_KNAVES 2
-#define OR_KNAVES 3
+#define KNIGHTS true
+#define KNAVES false
+#define CONJUNC 0
+#define DISJUNC 1
+
+// The 8 sentence types
+#define S_AT_LEAST 0
+#define S_AT_MOST 1
+#define S_EXACTLY 2
+#define S_ALL_OF_US 3
+#define S_I_AM 4
+#define S_SIR_IS_A 5
+#define S_DISJUNC_IS_A 6
+#define S_CONJUNC_IS_A 7
 
 
 typedef struct words {
@@ -26,9 +36,17 @@ typedef struct words {
     int close;
     // Semantics
     int speaker;    // Sir index.
-    int type;       // Sir listing type. 
+    int type;       // Sir listing type.
+    // Object(s)
+    int op;    // How the Sirs are collectivly grouped.
+    int sirs_start; // where the Sirs conjunction/disjunction starts. 
+    bool knights;
+    unsigned sirs;  // boolean array indexed as sirs[] 1=knight,0=knave
 } Sentence;
 
+
+unsigned sir_find_sirs(char * [], int, char *, char * [], int, unsigned); 
+int find_index(char *, char * [], int);
 
 /**
  * Is the given integer in the array (of int.s)?
@@ -55,6 +73,38 @@ int count_sirs(int i, char * text[]) {
     return count;
 }
 
+/**
+ * Find the sirs in a conjunction/disjunction phrase, single Sir, "I" and "All".
+ */
+void find_sirs(char * text[], Sentence * s, char * sirs[], int nb_of_sirs) {
+    if (s->sirs_start != -1) {
+        char * looking_for = (s->op == DISJUNC) ? "or" : "and";
+        s->sirs = sir_find_sirs(text, s->sirs_start + 1, looking_for, sirs, nb_of_sirs, 0);
+    }
+}
+/**
+ * Recurisvly build up a boolean array (unsigned int) with indexes matching those in the sirs[] array
+ */ 
+unsigned sir_find_sirs(char * text[], int current_i, char * delimiter, char * lookup[], int nb_of_sirs, unsigned output) {
+    if (strcmp(text[current_i], delimiter) != 0) {
+        int sir_i = find_index(text[current_i], lookup, nb_of_sirs);
+        if (sir_i != -1) {
+            output |= 1 << sir_i;
+            return sir_find_sirs(text, current_i + 1, delimiter, lookup, nb_of_sirs, output);
+        }
+    }
+    else {
+        // At the end so grab the last Sir and return.
+        int sir_i = find_index(text[current_i + 1], lookup, nb_of_sirs);
+        if (sir_i != -1)
+            output |= 1 << sir_i;
+    }
+    return output;
+}
+
+/**
+ * Finds the name of the speaker
+ */ 
 int find_speaker(char * text[], Sentence s) {
     for (int i = s.first; i <= s.last; i++)
         if (i < s.open || i > s.close)
@@ -63,6 +113,9 @@ int find_speaker(char * text[], Sentence s) {
     return -1;
 }
 
+/**
+ * finds the index of the given string in the the given array of strings
+ */
 int find_index(char * needle, char * haystack[], int size) {
     for (int i = 0; i < size; i++)
         if (strcmp(needle, haystack[i]) == 0)
@@ -70,6 +123,59 @@ int find_index(char * needle, char * haystack[], int size) {
     return -1;
 }
 
+/**
+ * Match the sentence to one of the 8 phrase templates and fill in the missing
+ * info in the Sentence struct passed.
+ */
+void match_phrase(Sentence * s, char * text[]) {
+    text[s->open][1] = tolower(text[s->open][1]);
+    // Are we talking about Knights or Knaves
+    s->knights = (strcmp(text[s->close], "Knight\"") == 0) ? true: false;
+    // Match sentence type...
+    if (strcmp(text[s->open], "\"at") == 0 ) {
+        if (strcmp(text[s->open + 1], "least") == 0) {
+            s->type = S_AT_LEAST;
+            s->op = CONJUNC;
+            s->sirs_start = s->open + 4;
+        }
+        else if (strcmp(text[s->open + 1], "most") == 0) {
+            s->type = S_AT_MOST;
+            s->op = CONJUNC;
+            s->sirs_start = s->open + 4;
+        }
+    } 
+    else if (strcmp(text[s->open], "\"exactly") == 0) {
+        s->type = S_EXACTLY;
+        s->op = CONJUNC;
+        s->sirs_start = s->open + 3;
+    }
+    else if (strcmp(text[s->open], "\"all") == 0) {
+        s->type = S_ALL_OF_US;
+        s->op = CONJUNC;
+        s->sirs_start = s->open;
+    }
+    else if (strcmp(text[s->open], "\"i") == 0) {
+        s->type = S_I_AM;    // This is ether a contradiction or a truisum...
+        s->op = -1;          // ... so we don't care what op it is.
+        s->sirs_start = s->open;
+    }
+    else if (strcmp(text[s->open], "\"sir") == 0 && strcmp(text[s->open + 2], "is")) {
+        s->type = S_SIR_IS_A;
+        s->op = CONJUNC;
+        s->sirs_start = s->open + 1;
+    }
+    // Tricky ones, match from the end.
+    else if (strcmp(text[s->close - 2], "is") == 0 && strcmp(text[s->close - 1], "a") == 0) {
+        s->type = S_DISJUNC_IS_A;
+        s->op = DISJUNC;
+        s->sirs_start = s->open;
+    }
+    else if (strcmp(text[s->close - 2], "are") == 0) {
+        s->type = S_CONJUNC_IS_A;
+        s->op = CONJUNC;
+        s->sirs_start = S->open;
+    }
+}
 
 
 /**
@@ -172,7 +278,7 @@ int main(int argc, char * argv[]) {
     Sentence sentences[nb_of_sentences];   // Sentences data.
     int current_begin = 1;  // coz argv inclues the program name at 0
     for (int s = 0; s < nb_of_sentences; s++) {
-        Sentence current_s = {current_begin, end_of_sentences[s], -1, -1, -1, -1};
+        Sentence current_s = {current_begin, end_of_sentences[s], -1, -1, -1, -1, -1, -1, -1, 0};
         for (int w = current_begin; w <= end_of_sentences[s]; w++) {
             // Look for quotes...
             if(argv[w][0] == '"') {
@@ -184,8 +290,12 @@ int main(int argc, char * argv[]) {
                             current_s.close = w;
                             break;
                         }
-                    if(current_s.close != -1)
+                    if(current_s.close != -1) {
+                        // Remove punctuation and move on...
+                        if(ispunct(argv[w][length - 1]))
+                            argv[w][length - 1] = '\0';
                         break;
+                    }
                 } while (++w <= end_of_sentences[s]); 
             }
         }
@@ -193,14 +303,18 @@ int main(int argc, char * argv[]) {
         sentences[s] = current_s;
     }
     // Look for Sir who is speaking.
-    for (int s = 0; s < nb_of_sentences; s++)
-        if (sentences[s].open != -1) {
-            int speaker_i = find_speaker(argv, sentences[s]);
+    for (int i = 0; i < nb_of_sentences; i++) {
+        Sentence * s = &sentences[i];
+        if (s->open != -1) {
+            int speaker_i = find_speaker(argv, *s);
             if(speaker_i != -1)
-                sentences[s].speaker = find_index(argv[speaker_i], sirs, nb_of_sirs);
+                s->speaker = find_index(argv[speaker_i], sirs, nb_of_sirs);
+            // match phrase to one of the 8 templates.
+            match_phrase(s, argv);
+            find_sirs(argv, s, sirs, nb_of_sirs);
         }
-    // match phrase to one of the 8 templates.
-
+    }
+    
     
     
     
